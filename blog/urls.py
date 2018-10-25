66011ef44085dc1c66850af5f480978409ca4ad2
AlejandroSi <al.silvap@alumnos.duoc.cl>
2018-10-25 14:31:05 -0300

    enlaza paginas, agrega vistas y url enlaza template html5, vistas y url python

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('mascota', views.mascotas, name='mascota'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('adopta', views.adopta, name='adopta'),
+    path('contacto', views.contacto, name='contacto'),
+
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,12 @@ from .models import Post
 def index(request):
     return render(request, 'blog/index.html',)
 
+def mascotas(request):
+    return render(request, 'blog/mascotas.html',)
+def nosotros(request):
+    return render(request, 'blog/nosotros.html',)
+def adopta(request):
+    return render(request, 'blog/adopta.html',)
+def contacto(request):
+    return render(request, 'blog/contacto.html',)
 # Create your views here.
