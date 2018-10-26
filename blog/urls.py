da69af51f802fe263e4627066c6233a295c394e7
AlejandroSi <al.silvap@alumnos.duoc.cl>
2018-10-26 10:32:43 -0300

    arregla js y agrega perros rescatados en el index se agrega perros rescatados con descripcion

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('mascota', views.mascotas, name='mascota'),
     path('nosotros', views.nosotros, name='nosotros'),
     path('adopta', views.adopta, name='adopta'),
